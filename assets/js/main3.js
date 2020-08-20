b7e74dc542b97037554fe84b87b11faf14a7d38e
Cyber Asad <cyber.asad24@gmail.com>
2020-08-20 15:22:06 +0600

    Isotope mechanism done with jQuery without any plugin at index3.html

--- assets/js/main3.js
+++ assets/js/main3.js
@@ -3,73 +3,44 @@
 
     jQuery(document).ready(function ($) {
 
-        var $isotopeSelectors = $(".isotope-button");
 
-        $isotopeSelectors.each(function() {
-            var $selector = $(this);
-            var $selectorData = $selector.data('isotope');
+        // Isotope Mechanism Start
+        var $isotopeSelectors = $(".isotope-button"); // Isotope Selectors
 
-            $selector.click(function () {
+        // Looping through each isotope selectors
+        $isotopeSelectors.each(function () {
+            var $selectorSelf = $(this);    // Isotope selector
+            var $selectorNumber = $selectorSelf.data('isotope');    // Selector's data number
+
+            $selectorSelf.click(function () {
 
                 //    Add/Remove Active Class from Isotope Selectors
                 $($isotopeSelectors).removeClass("active");
                 $(this).addClass("active");
 
+                var $parentWrapper = $selectorSelf.closest(".isotope-wrapper"); // Parent wrapper
+                var $isotopeItems = $parentWrapper.find(".isotope-item");       // Isotope items
 
-                //    Switching Isotope item by Clicking Tab Items
-                var $isotopeItems = $(".isotope-item");
-                $isotopeItems.each(function() {
-                    var $singleItem = $(this);
-                    console.log($singleItem);
+                // Looping through each isotope items inside the parent wrapper
+                $isotopeItems.each(function () {
+                    var $itemSelf = $(this);    // Isotope item
+                    var $itemNumber = $itemSelf.data('isotope');    // Selector's data number
 
-                    var $itemData = $singleItem.data('isotope');
-                    
-                    if($selectorData !== $itemData) {
-                        $singleItem.hide();
+                    if ($selectorNumber === $itemNumber || $selectorNumber === 0) {
+                        $itemSelf.show(500);
                     }
                     else {
-                        $singleItem.show();
+                        $itemSelf.hide();
                     }
+
                 });
 
             });
 
-            
-
         });
-        
-        //    Add/Remove Active Class from Isotope Selectors
-        // $(".isotope-button").click(function () {
-        //     $(".isotope-button").removeClass("active");
-        //     $(this).addClass("active");
-        // });
+        // Isotope Mechanism End
 
-        // //    Switching Isotope item by Clicking Tab Items
-        // $(".isotope-button-0").click(function () {
-        //     // $(".isotope-item").addClass("active");
-        //     $(".isotope-item").show(500);
-        // });
 
-        // $(".isotope-button-1").click(function () {
-        //     // $(".isotope-item").removeClass("active");
-        //     // $(".isotope-item-1").addClass("active");
-        //     $(".isotope-item").hide();
-        //     $(".isotope-item-1").show(500);
-        // });
-
-        // $(".isotope-button-2").click(function () {
-        //     // $(".isotope-item").removeClass("active");
-        //     // $(".isotope-item-2").addClass("active");
-        //     $(".isotope-item").hide();
-        //     $(".isotope-item-2").show(500);
-        // });
-
-        // $(".isotope-button-3").click(function () {
-        //     // $(".isotope-item").removeClass("active");
-        //     // $(".isotope-item-3").addClass("active");
-        //     $(".isotope-item").hide();
-        //     $(".isotope-item-3").show(500);
-        // });
 
 
     });
