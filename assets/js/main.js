a6e99e85f9819ba05d57eb7600f06324823e4243
iqbalrony <iqbalrony30@gmail.com>
2020-08-20 13:17:48 +0600

    testing append into isotop markpup

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,12 @@
     jQuery(window).load(function () {
 
         // Activate isotope in container
-        $(".isotope-items").isotope({
+        var $grid = $(".isotope-items");
+        $grid.isotope({
             itemSelector: '.isotope-item',
-            masonry: {
-              columnWidth: 0
-            }
+            // masonry: {
+            //   columnWidth: 0
+            // }
         });
 
         // Add isotope click function
@@ -17,7 +18,7 @@
             $(this).addClass("active");
 
             var selector = $(this).attr('data-filter');
-            $(".isotope-items").isotope({
+            $grid.isotope({
                 filter: selector,
                 animationOptions: {
                     duration: 750,
@@ -26,6 +27,38 @@
                 }
             });
             return false;
+        });
+
+        // Add isotope click function
+        $(".load-more").click(function (e) {
+            e.preventDefault();
+
+            // $(".isotope-button").removeClass("active");
+            // $(this).addClass("active");
+
+            console.log('some');
+            var elem = $("<div class='isotope-item one'></div>").text("Text.");
+            var elem2 = "<div class='isotope-item one'><h1>Appended Markup</h1></div>";
+
+            console.log( typeof elem, typeof elem2 );
+            console.log( elem, elem2 );
+            var html = $.parseHTML( elem2 );
+            // $grid.isotope()
+            // .append( elem )
+            // .isotope( 'appended', elem )
+            // .isotope('layout');
+
+            //elem.appendTo( $grid );
+            $grid.append( html )
+            // $grid.isotope('appended', elem);
+            // $grid.isotope('layout');
+            setTimeout( function(){
+                console.log('i am in');
+                // $grid.isotope('layout');
+                $grid.isotope( 'appended', html ).isotope('layout');
+            },1000);
+            // $grid.isotope().isotope( 'appended', elem ).isotope('layout');
+            // $grid.isotope( 'appended', elem ).isotope('layout');
         });
 
     });
