0d2859359835531b76ea6fd9dceadb2a62584cc1
iqbalrony <iqbalrony30@gmail.com>
2020-08-22 13:26:47 +0600

    update style two

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,51 +1,61 @@
 (function ($) {
-    "use strict";
+	"use strict";
 
-    jQuery(document).ready(function ($) {
-        
-        //    Add/Remove Active Class from Isotope Selectors
-        $(".isotope-button").click(function () {
-            $(".isotope-button").removeClass("active");
-            $(this).addClass("active");
+	jQuery(document).ready(function($){
+
+		// Custom Filter 
+        var $isotopeSelectors = $(".xyz-masonry-filter-item");
+        $isotopeSelectors.each(function () {
+            var $selectorSelf = $(this);
+
+            $selectorSelf.click(function () {
+				var $self = $(this);
+				var $dataText = $self.data('filter').replace(".", "");
+
+				$self.closest(".xyz-masonry-filter").find(".xyz-masonry-filter-item").removeClass("active");
+                $self.addClass("active");
+
+                var $parentWrapper = $self.closest(".xyz-custom-post-type-wrapper");
+                var $isotopeItems = $parentWrapper.find(".xyz-masonry-item");
+                
+                $isotopeItems.each(function () {
+                    var $itemSelf = $(this);
+                    var $itemHasCalss = $itemSelf[0].classList.contains($dataText);
+
+                    if ( $itemHasCalss || $dataText === '*') {
+                        $itemSelf.show(500);
+                    }
+                    else {
+                        $itemSelf.hide();
+                    }
+
+                });
+
+            });
         });
 
-        //    Switching Isotope item by Clicking Tab Items
-        $(".isotope-button-0").click(function () {
-            // $(".isotope-item").addClass("active");
-            $(".isotope-item").show(500);
-        });
+		// Load More Ajax
+		var button = $('.xyz-load-more');
+		button.on("click", function(e) {
+			e.preventDefault();
+			var $self = $(this),
+			query_settings = $self.data("settings"),
+			$button_wrap = $self.closest(".xyz-load-more-wrap"),
+			$main_wrapper = $self.closest(".xyz-custom-post-type-wrapper"),
+			$masonry_wrapper =  $main_wrapper.find('.xyz-masonry-container'),
+			item = $main_wrapper.find('.xyz-masonry-item'),
+			loaded_item = $main_wrapper.find('.xyz-masonry-item').length;
 
-        $(".isotope-button-1").click(function () {
-            // $(".isotope-item").removeClass("active");
-            // $(".isotope-item-1").addClass("active");
-            $(".isotope-item").hide();
-            $(".isotope-item-1").show(500);
-        });
+			$self.attr("disabled", true);
+			var push_item = '<div class="xyz-masonry-item xyz-is-filter-two"><img src="https://picsum.photos/400/300" alt=""><h3><a href="#">Lorem ipsum dolor sit amet consectetur</a></h3><ul><li><a href="#">Webiner</a></li><li><a href="#">28 May 2020</a></li></ul><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, in. Beatae alias modi fugit laborum ex illum<span>...</span></p><a href="#">5 min read</a></div>';
+			
+			var html = $.parseHTML( push_item );
+			$masonry_wrapper.append( html );
+			$self.removeAttr('disabled');
 
-        $(".isotope-button-2").click(function () {
-            // $(".isotope-item").removeClass("active");
-            // $(".isotope-item-2").addClass("active");
-            $(".isotope-item").hide();
-            $(".isotope-item-2").show(500);
-        });
-
-        $(".isotope-button-3").click(function () {
-            // $(".isotope-item").removeClass("active");
-            // $(".isotope-item-3").addClass("active");
-            $(".isotope-item").hide();
-            $(".isotope-item-3").show(500);
-        });
+		});
 
 
-    });
+	});
 
-
-
-
-    jQuery(window).load(function () {
-
-
-    });
-
-
-}(jQuery));
+})(jQuery);
